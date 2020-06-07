f3b34ea87bef75e4911b76725492f1cdf1959d01
Richard Wallis <rdwallis@gmail.com>
2020-06-07 10:23:31 +0200

    Add keyweight method to url info

--- src/main/java/com/wallissoftware/byos/pack/UrlInfo.java
+++ src/main/java/com/wallissoftware/byos/pack/UrlInfo.java
@@ -3,6 +3,7 @@ package com.wallissoftware.byos.pack;
 import lombok.Data;
 import lombok.Value;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Data
@@ -12,4 +13,12 @@ public class UrlInfo {
   private String title;
   private String description;
   private Map<String, Long> keyWeights;
+
+  public void addKeyWeight(String key, Long weight) {
+    if (keyWeights == null) {
+      keyWeights = new HashMap<>();
+    }
+    keyWeights.put(key, keyWeights.getOrDefault(key, 0L) + weight);
+  }
+
 }
